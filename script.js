539e1ce5f93e091e11e8e35184c0c76d8b6f19fb
Muhammad Rayyan <154331886+MuhammadRayyan16@users.noreply.github.com>
2025-02-06 07:43:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
 
-
 document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + "px";
   crsr.style.top = dets.y + "px";
